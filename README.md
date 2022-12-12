5620a95f8720181f504bde371bf3c132048171fd
Matej Capkovic <mcapkovic@gmail.com>
2022-12-12 07:32:44 +0100

    add day 11 p2

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%86/yellow)](src/day11)
-[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%86/yellow)](src/day12)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -146,9 +146,9 @@ Both parts: 0.811167ms
 
 ```
 Day 12
-Time part 1: 5.362ms
-Time part 2: -
-Both parts: 5.362208ms
+Time part 1: -
+Time part 2: 429.932ms
+Both parts: 429.9315ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 21/50
-Total time: 38.141ms
+Total stars: 22/50
+Total time: 462.71ms
 ```
 
 <!--/RESULTS-->
--- src/day12/index.js
+++ src/day12/index.js
@@ -70,8 +70,71 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  let endPosition
+  let startingPositions = []
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[row].length; col++) {
+      if (input[row][col] === "S") {
+        startingPositions.push([row, col])
+        continue
+      }
+      if (input[row][col] === "a") {
+        startingPositions.push([row, col])
+        continue
+      }
+      if (input[row][col] === "E") {
+        endPosition = [row, col]
+        continue
+      }
+    }
+  }
 
-  return
+  const allDistances = startingPositions.map((startPosition) => {
+    const queue = [startPosition]
+    const visited = new Set()
+    const distances = new Map()
+    distances.set(`${startPosition[0]}-${startPosition[1]}`, 0)
+    visited.add(`${startPosition[0]}-${startPosition[1]}`)
+
+    while (queue.length > 0) {
+      const [row, col] = queue.shift()
+      const currentDistance = distances.get(`${row}-${col}`)
+      const currentElevation = ELEVATIONS.indexOf(input[row][col])
+      const neighbors = [
+        [row - 1, col],
+        [row + 1, col],
+        [row, col - 1],
+        [row, col + 1],
+      ]
+      neighbors.forEach(([neighborRow, neighborCol]) => {
+        if (
+          neighborRow < 0 ||
+          neighborRow >= input.length ||
+          neighborCol < 0 ||
+          neighborCol >= input[0].length
+        )
+          return
+        let elevationSymbol = input[neighborRow][neighborCol]
+        if (elevationSymbol === "E") elevationSymbol = "z"
+        const neighborElevation = ELEVATIONS.indexOf(elevationSymbol)
+        if (neighborElevation > currentElevation + 1) return
+
+        if (visited.has(`${neighborRow}-${neighborCol}`)) return
+        visited.add(`${neighborRow}-${neighborCol}`)
+        distances.set(`${neighborRow}-${neighborCol}`, currentDistance + 1)
+        queue.push([neighborRow, neighborCol])
+      })
+    }
+
+    const endPositionDistance = distances.get(
+      `${endPosition[0]}-${endPosition[1]}`,
+    )
+    if (!endPositionDistance) return Infinity
+
+    return endPositionDistance
+  })
+
+  return Math.min(...allDistances)
 }
 
 const part1Config = {
@@ -88,7 +151,7 @@ const part2Config = {
   tests: [
     {
       input: example1,
-      expected: "",
+      expected: 29,
     },
   ],
   solution: part2,
@@ -96,7 +159,7 @@ const part2Config = {
 
 run({
   part1: part1Config,
-  // part2: part2Config,
+  part2: part2Config,
   trimTestInputs: true,
   // onlyTests: true,
   onlyTests: false,
